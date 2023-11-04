7423fcf8362dd1fee7727ab1e17664072cf74908
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-04 11:43:42 -0700

    add catagory routes

---
+++ route/catagoryRoute.js
@@ -0,0 +1,21 @@
+import express from 'express'
+const router = express.Router()
+
+router.get('/catagories',(req,res)=> {
+    res.send('get all catagories')
+})
+router.get('/catagory:number',(req,res) => {
+    console.log(req.param.number)
+    res.send(req.param.number)
+})
+router.post('/catagory',(req,res) => {
+    res.send(req.body)
+})
+router.put('/catagory',(req,res)=> {
+    res.send('data updated succssfuly')
+})
+router.delete('/catagory',(req,res) => {
+    res.send('data deleted successfully')
+})
+
+module.exports = router
