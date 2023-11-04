ffe9e34181403cdfd40add18eeaf801622340197
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-04 11:43:59 -0700

    separate the routes code from the server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+import {router} from "./route/catagoryRoute.js"
 const port = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
-app.use('/api')
-app.get('/',(req,res) => {
-    res.json({message : "wg"})
-})
+app.use('/api',router)
+
 app.listen(5000,(req,res)=> {
     console.log("what is happening here")
 })
