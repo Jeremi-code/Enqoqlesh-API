2f1bd7cbe91c44b5f112f1be8e84355af34d5176
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-05 23:49:06 -0800

    use the two route middleware

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
-import {router} from "./route/category.js"
+import categoryRouter from "./route/category.js"
+import questionRouter from "./route/question.js"
 const port = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
-app.use('/api',router)
+app.use(categoryRouter)
+app.use(questionRouter)
 
 app.listen(port,(req,res)=> {
     console.log(`server running ${port}`)
