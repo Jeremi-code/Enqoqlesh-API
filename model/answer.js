939dc8bf3b23980e5a4a4400893d5d1a3fa6c38b
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-06 04:01:46 -0800

    Add answer model with required fields and timestamps.

---
+++ model/answer.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+const answerSchema = mongoose.Schema ({
+    answer: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now()
+    }
+})
+
+const answerModel = mongoose.model('Answer', answerSchema)
