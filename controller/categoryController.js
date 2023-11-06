a22b23362efe18f64242f6630ba8b3d35467d839
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-06 04:24:40 -0800

    Add CRUD functionality for category model in categoryController.js

---
+++ controller/categoryController.js
@@ -0,0 +1,59 @@
+import Category from '../models/category.js';
+
+const getCategories = async (req, res) => {
+    try {
+        const categories = await Category.find();
+        res.status(200).json(categories);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
+const getCategoryById = async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category); 
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+const registerCategory = async (req, res) => {
+    try {
+        const { name, description } = req.body;
+        const category = new Category({
+            name,
+            description
+        });
+        const createdCategory = await category.save();
+        res.status(201).json(createdCategory);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+const updateCategory = async (req, res) => {
+    try {
+        const { name, description } = req.body;
+        const category = await Category.findById(req.params.id);
+        if (category) {
+            category.name = name;
+            category.description = description;
+        }
+        const updatedCategory = await category.save();
+        res.status(200).json(updatedCategory);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+const deleteCategory = async (req,res) => {
+    try {
+        const category = await Category.findById(req.params.id);
+        if(category) {
+            category.remove();
+            res.status(200).json({ message: 'Category deleted successfully' });
+        }
+        else {
+            res.status(404).json({ message: 'Category not found' });
+        }
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
