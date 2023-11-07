92b49c9dc08ee2998a4951b4f7c43ab6dfe0b2fc
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-07 05:16:49 -0800

    Add registerAnswer function to answerController.jsf for adding answers

--- controller/answerController.js
+++ controller/answerController.js
@@ -21,3 +21,18 @@ const getAnswers = async (req, res) => {
         res.status(404).json({ message: error.message });
     }
 }
+const registerAnswer = async (req, res) => {
+    try {
+        const { text, description } = req.body;
+        const answer = new AnswerModel({
+            text,
+            description,
+            createdAt: new Date().toISOString(),
+            updateAt: new Date().toISOString()
+        });
+        const createdAnswer = await answer.save();
+        res.status(201).json(createdAnswer);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
