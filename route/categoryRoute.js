88f5ca88b33a5eb3dd8e5b0af07eb8702f05d6f9
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-10 05:43:45 -0800

    Add apiKeyMiddleware to categoryRoute.js

--- route/categoryRoute.js
+++ route/categoryRoute.js
@@ -6,11 +6,12 @@ import {
   registerCategory,
   deleteCategory,
 } from "../controller/categoryController.js";
+import { apiKeyMiddleware} from "../middleware/authMiddleware.js";
 const categoryRouter = express.Router();
 
 categoryRouter.get("/categories", getCategories);
 categoryRouter.get("/category/:category", getCategory);
-categoryRouter.post("/category", registerCategory);
+categoryRouter.post("/category",apiKeyMiddleware, registerCategory);
 categoryRouter.put("/category/:category", updateCategory);
 categoryRouter.delete("/category/:category", deleteCategory);
 
