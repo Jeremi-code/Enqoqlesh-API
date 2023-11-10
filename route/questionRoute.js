78e823bb7cc108caff34c4a11ca7aee414460b4d
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-10 05:49:46 -0800

    Add API key middleware to question routes

--- route/questionRoute.js
+++ route/questionRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { getQuestions,getQuestion,registerQuestion,updateQuestion,deleteQuestion} from '../controller/questionController.js'
+import { apiKeyMiddleware} from "../middleware/authMiddleware.js";
 const questionRouter = express.Router()
 
 questionRouter.get('/category/:category/questions',getQuestions)
 questionRouter.get('/category/:category/question/:id',getQuestion)
-questionRouter.post('/category/:category/question',registerQuestion)
-questionRouter.put('/category/:category/question/:id',updateQuestion)
-questionRouter.delete('/category/:category/question/:id',deleteQuestion)
+questionRouter.post('/category/:category/question', apiKeyMiddleware, registerQuestion)
+questionRouter.put('/category/:category/question/:id', apiKeyMiddleware, updateQuestion)
+questionRouter.delete('/category/:category/question/:id', apiKeyMiddleware, deleteQuestion)
 
 export default questionRouter
