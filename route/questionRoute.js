5cd705c84d957f56190885b833644c69368f5c33
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-13 12:19:04 -0800

    change the middleware used in this route

--- route/questionRoute.js
+++ route/questionRoute.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { getQuestions,getQuestion,registerQuestion,updateQuestion,deleteQuestion} from '../controller/questionController.js'
-import { apiKeyMiddleware} from "../middleware/authMiddleware.js";
+import { defaultRouteMiddleware} from "../middleware/authMiddleware.js";
 const questionRouter = express.Router()
 
 questionRouter.get('/category/:category/questions',getQuestions)
 questionRouter.get('/category/:category/question/:id',getQuestion)
-questionRouter.post('/category/:category/question', apiKeyMiddleware, registerQuestion)
-questionRouter.put('/category/:category/question/:id', apiKeyMiddleware, updateQuestion)
-questionRouter.delete('/category/:category/question/:id', apiKeyMiddleware, deleteQuestion)
+questionRouter.post('/category/:category/question', defaultRouteMiddleware, registerQuestion)
+questionRouter.put('/category/:category/question/:id', defaultRouteMiddleware, updateQuestion)
+questionRouter.delete('/category/:category/question/:id', defaultRouteMiddleware, deleteQuestion)
 
 export default questionRouter
