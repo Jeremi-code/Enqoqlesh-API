ea3006d8cac869c3dc9dc7876f8d4db0cca26c44
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-13 11:52:37 -0800

    add the routes and the functions to be used in that route

--- route/adminRoute.js
+++ route/adminRoute.js
@@ -1,3 +1,8 @@
 import express from 'express'
+import { addAdmin,removeAdmin } from '../controller/adminController'
 const adminRouter = express.Router
 
+adminRouter.post('/admin',addAdmin)
+adminRouter.delete('/admin/apiKey',removeAdmin)
+
+export default adminRouter
