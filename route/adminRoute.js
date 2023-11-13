9ccda7847ed12c250dc93635d00170236006c961
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-13 11:56:41 -0800

    add middleware inside admin routes

--- route/adminRoute.js
+++ route/adminRoute.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { addAdmin,removeAdmin } from '../controller/adminController'
+import { apiKeyMiddleware } from '../middleware/authMiddleware'
 const adminRouter = express.Router
 
-adminRouter.post('/admin',addAdmin)
-adminRouter.delete('/admin/apiKey',removeAdmin)
+adminRouter.post('/admin',apiKeyMiddleware,addAdmin)
+adminRouter.delete('/admin/apiKey',apiKeyMiddleware,removeAdmin)
 
 export default adminRouter
