622da3c0c962efbb29e9f936448048096c237f76
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-13 11:58:03 -0800

    catch edge case

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const apiKeyMiddleware = (req, res, next) => {
     const apiKey = req.headers['api-key'];
-    if (apiKey !== process.env.API_KEY) {
+    if (apiKey !== process.env.API_KEY || apiKey == undefined) {
         return res.status(401).json({
         message: 'Unauthorized',
         });
