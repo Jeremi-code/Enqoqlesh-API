c948f621ab21b4051bff333fefdef55c268b9e7d
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-13 12:18:10 -0800

    change variable and add another middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,23 @@
-const apiKeyMiddleware = (req, res, next) => {
-    const apiKey = req.headers['api-key'];
-    if (apiKey !== process.env.API_KEY || apiKey == undefined) {
+import AdminModel from '../model/admin.js'
+const adminRouteMiddleware = (req, res, next) => {
+    const API_KEY = req.headers['api-key'];
+    if (API_KEY !== process.env.API_KEY || API_KEY == undefined) {
         return res.status(401).json({
         message: 'Unauthorized',
         });
     }
     next();
     }
+const defaultRouteMiddleware = (req,res,next) => {
+    const API_KEY = req.headers['api-key']
+    const admin =  AdminModel.findOne({apiKey:API_KEY})
+    if (admin )
+        next ()
+    else {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        })
+    }
+}
 
-
-export { apiKeyMiddleware}
+export { adminRouteMiddleware,defaultRouteMiddleware}
