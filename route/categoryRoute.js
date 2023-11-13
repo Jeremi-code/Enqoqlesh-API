1b39bf19a632fa9a382573f343f5b45ea6a08b69
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-13 12:18:56 -0800

    change the middleware used in this route

--- route/categoryRoute.js
+++ route/categoryRoute.js
@@ -6,13 +6,13 @@ import {
   registerCategory,
   deleteCategory,
 } from "../controller/categoryController.js";
-import { apiKeyMiddleware} from "../middleware/authMiddleware.js";
+import { defaultRouteMiddleware} from "../middleware/authMiddleware.js";
 const categoryRouter = express.Router();
 
 categoryRouter.get("/categories", getCategories);
 categoryRouter.get("/category/:category", getCategory);
-categoryRouter.post("/category", apiKeyMiddleware, registerCategory);
-categoryRouter.put("/category/:category", apiKeyMiddleware, updateCategory);
-categoryRouter.delete("/category/:category", apiKeyMiddleware, deleteCategory);
+categoryRouter.post("/category", defaultRouteMiddleware, registerCategory);
+categoryRouter.put("/category/:category", defaultRouteMiddleware, updateCategory);
+categoryRouter.delete("/category/:category", defaultRouteMiddleware, deleteCategory);
 
 export default categoryRouter;
