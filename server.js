cac8e738cf53b234906abbc863c1f79c540d6d81
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-05 11:14:05 -0800

    format the code

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-import categoryRouter from "./route/categoryRoute.js"
-import questionRouter from "./route/questionRoute.js"
-import connectDB from './config/db.js'
-import adminRouter from './route/adminRoute.js'
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import categoryRouter from "./route/categoryRoute.js";
+import questionRouter from "./route/questionRoute.js";
+import connectDB from "./config/db.js";
+import adminRouter from "./route/adminRoute.js";
 
-connectDB()
+connectDB();
 
-const PORT = process.env.PORT || 5000
-const app = express()
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-app.use(express.json())
-app.use(categoryRouter)
-app.use(questionRouter)
-app.use(adminRouter)
+app.use(express.json());
+app.use(categoryRouter);
+app.use(questionRouter);
+app.use(adminRouter);
 
-app.listen(PORT,()=> {
-    console.log(`Server running on PORT: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+});
