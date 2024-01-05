1d8564a76a5b1a4a273432ad738ddd77016f9623
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-04 23:50:59 -0800

    Refactor authMiddleware.js for better readability and error handling

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,23 +1,23 @@
-import AdminModel from '../model/admin.js'
+import AdminModel from "../model/admin.js";
 const adminRouteMiddleware = (req, res, next) => {
-    const API_KEY = req.headers['api-key'];
-    if (API_KEY !== process.env.API_KEY || API_KEY == undefined) {
-        return res.status(401).json({
-        message: 'Unauthorized',
-        });
-    }
-    next();
-    }
-const defaultRouteMiddleware = (req,res,next) => {
-    const API_KEY = req.headers['api-key']
-    const admin =  AdminModel.findOne({apiKey:API_KEY})
-    if (admin )
-        next ()
-    else {
-        return res.status(401).json({
-            message: 'Unauthorized'
-        })
-    }
-}
+  const API_KEY = req.headers["api-key"];
+  if (API_KEY !== process.env.API_KEY || API_KEY == undefined) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+  next();
+};
+const defaultRouteMiddleware = async (req, res, next) => {
+  const API_KEY = req.headers["api-key"];
+  const admin = await AdminModel.findOne({ apiKey: API_KEY });
+  console.log(admin);
+  if (admin) next();
+  else {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+};
 
-export { adminRouteMiddleware,defaultRouteMiddleware}
+export { adminRouteMiddleware, defaultRouteMiddleware };
