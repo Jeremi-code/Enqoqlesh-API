1036f0abcddd56f6997cd4dfadc5b5b372828b36
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-05 11:09:58 -0800

    change the name of registerCategory function into addCategory

--- route/categoryRoute.js
+++ route/categoryRoute.js
@@ -3,7 +3,7 @@ import {
   getCategories,
   getCategory,
   updateCategory,
-  registerCategory,
+  addCategory,
   deleteCategory,
 } from "../controller/categoryController.js";
 import { defaultRouteMiddleware} from "../middleware/authMiddleware.js";
@@ -11,7 +11,7 @@ const categoryRouter = express.Router();
 
 categoryRouter.get("/categories", getCategories);
 categoryRouter.get("/category/:category", getCategory);
-categoryRouter.post("/category", defaultRouteMiddleware, registerCategory);
+categoryRouter.post("/category", defaultRouteMiddleware, addCategory);
 categoryRouter.put("/category/:category", defaultRouteMiddleware, updateCategory);
 categoryRouter.delete("/category/:category", defaultRouteMiddleware, deleteCategory);
 
